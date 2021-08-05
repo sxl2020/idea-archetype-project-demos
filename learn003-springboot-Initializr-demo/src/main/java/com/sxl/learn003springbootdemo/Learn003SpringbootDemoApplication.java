6271d697c27818cc098d7274407292dd41e84fc2
sxl2020 <suqiancheng_su@163.com>
2021-08-05 23:13:02 +0800

    Initial commit

---
+++ learn003-springboot-Initializr-demo/src/main/java/com/sxl/learn003springbootdemo/Learn003SpringbootDemoApplication.java
@@ -0,0 +1,13 @@
+package com.sxl.learn003springbootdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Learn003SpringbootDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Learn003SpringbootDemoApplication.class, args);
+    }
+
+}
---
+++ learn003-springboot-Initializr-demo/src/test/java/com/sxl/learn003springbootdemo/Learn003SpringbootDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sxl.learn003springbootdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Learn003SpringbootDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
